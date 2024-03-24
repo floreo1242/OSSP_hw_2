87e3c02953a85700725c3613c924ac571a38e401
Jinsoo Yoon <floreo1242@gmail.com>
2024-03-24 19:56:00 +0900

    2024-03-20 OSSP 개인과제#2

---
+++ name-id.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string name, id;
+    cout << "이름을 입력하세요: ";
+    cin >> name;
+    cout << "학번을 입력하세요: ";
+    cin >> id;
+    cout << "\n<출력>\n";
+    cout << "이름: " << name << "\n";
+    cout << "학번: " << id << "\n";
+    return 0;
+}
